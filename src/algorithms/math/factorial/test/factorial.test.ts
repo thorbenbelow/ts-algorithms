721fbc56b814d760897bd908d5e654214a1d6780
Thorben Below <thorbenbelow@gmail.com>
2020-08-29 15:34:25 +0200

    fixed lint findings

--- src/algorithms/math/factorial/test/factorial.test.ts
+++ src/algorithms/math/factorial/test/factorial.test.ts
@@ -1,12 +1,13 @@
 import {factorial, factorialRecursive} from "../factorial";
 
-function assertFactorial(factorial: (n:number) => number){
+function assertFactorial(factorial: (n: number) => number): void {
   expect(factorial(0)).toBe(1);
   expect(factorial(1)).toBe(1);
   expect(factorial(5)).toBe(120);
   expect(factorial(8)).toBe(40320);
   expect(factorial(10)).toBe(3628800);
 }
+
 describe('factorial', () => {
   it('should calculate factorial', () => {
     assertFactorial(factorial);
