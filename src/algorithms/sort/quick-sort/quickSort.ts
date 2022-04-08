c703a6df30d829b1b4885c42de4f49307e42648c
Thorben Below <below.thorben@inside-online.de>
2022-04-08 19:28:57 +0200

    add in place quicksort

--- src/algorithms/sort/quick-sort/quickSort.ts
+++ src/algorithms/sort/quick-sort/quickSort.ts
@@ -1,26 +1,49 @@
 export function quickSort(A: number[]): number[] {
-  if(A.length <= 1){ 
+  if (A.length <= 1) {
     return A;
   }
-  
+
   const pivot = A[0];
-  const left=[], right= [], mid = [];
-  
-  for(const x of A){
-    if(x < pivot) {
+  const left = [], right = [], mid = [];
+
+  for (const x of A) {
+    if (x < pivot) {
       left.push(x);
-    }else if(x> pivot) {
+    } else if (x > pivot) {
       right.push(x);
-    }else {
+    } else {
       mid.push(x);
     }
   }
-  
+
   return [...quickSort(left), ...mid, ...quickSort(right)]
 }
 
 
-export function quickSortInPlace<T = unknown>(A: Array<T>): void {
-  console.log(A);
-  // TODO
+export function quickSortInPlace<T = unknown>(A: Array<T>, lo = 0, hi = A.length - 1): void {
+  if (lo >= hi || lo < 0) {
+    return;
+  }
+
+  const p = partition(A, lo, hi);
+
+  quickSortInPlace(A, lo, p - 1);
+  quickSortInPlace(A, p + 1, hi);
+
+}
+
+function partition<T = unknown>(A: Array<T>, lo: number, hi: number): number {
+  const pivot = A[hi];
+
+  let i = lo - 1;
+
+  for (let j = lo; j < hi; j++) {
+    if (A[j] <= pivot) {
+      i++;
+      [A[i], A[j]] = [A[j], A[i]];
+    }
+  }
+  i++;
+  [A[i], A[hi]] = [A[hi], A[i]];
+  return i;
 }
--- src/algorithms/sort/quick-sort/test/quickSort.test.ts
+++ src/algorithms/sort/quick-sort/test/quickSort.test.ts
@@ -2,12 +2,12 @@ import { quickSort, quickSortInPlace } from "../quickSort";
 
 describe('quicksort', () => {
   it('should return a sorted array', () => {
-    expect(quickSort([3,2,5,2,1])).toEqual([1,2,2,3,5]);
+    expect(quickSort([3, 2, 5, 2, 1])).toEqual([1, 2, 2, 3, 5]);
   })
 
-  xit('should sort the array in place', () => {
-    const A = [3,2,5,2,1];
+  it('should sort the array in place', () => {
+    const A = [3, 2, 5, 2, 1];
     quickSortInPlace(A);
-    expect(A).toEqual([1,2,2,3,5]);
+    expect(A).toEqual([1, 2, 2, 3, 5]);
   })
 });
